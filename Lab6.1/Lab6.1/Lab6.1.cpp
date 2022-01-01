ab75bb43ede74f3c056db5a69e09db41b2723bea
popidiot <90963991+popidiot@users.noreply.github.com>
2022-01-01 20:55:26 +0300

    Add files via upload

---
+++ Lab6.1/Lab6.1/Lab6.1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    stack <char> check;
+    string input;
+    cout << "Input string: ";
+    cin >> input;
+    for (int i = 0; i < input.size(); i++) {
+        if (input[i] == '(' || input[i] == '[' || input[i] == '{') {
+            check.push(input[i]);
+        }
+        else if (input[i] == ')' || input[i] == ']' || input[i] == '}') {
+            if (check.empty()) {
+                cout << "Wrong";
+                return 1;
+            }
+            switch (input[i]) {
+            case ')':
+                if (check.top() == '(') check.pop();
+                else {
+                    cout << "Wrong";
+                    return 1;
+                }
+                break;
+            case ']':
+                if (check.top() == '[') check.pop();
+                else {
+                    cout << "Wrong";
+                    return 1;
+                }
+                break;
+            case '}':
+                if (check.top() == '{') check.pop();
+                else {
+                    cout << "Wrong";
+                    return 1;
+                }
+                break;
+            }
+        }
+    }
+    cout << "Right." << endl;
+    return 0;
+}
