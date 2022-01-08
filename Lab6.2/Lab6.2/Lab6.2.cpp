af639976aeaad064eb06adaac0184ecf8d0bb689
popidiot <90963991+popidiot@users.noreply.github.com>
2022-01-08 18:26:59 +0300

    Add files via upload

---
+++ Lab6.2/Lab6.2/Lab6.2.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <deque>
+
+using namespace std;
+
+bool palindrom() {
+    string S;
+    deque <char> check;
+    cout << "Type string: ";
+    cin >> S;
+    for (int i = 0; i < S.size(); i++) {
+        check.push_back(S[i]);
+    }
+    while (!check.empty()) {
+        if (check.size() == 1) break;
+        if (check.front() == check.back()) {
+            check.pop_back();
+            check.pop_front();
+        }
+        else {
+            cout << "Not palindrom" << endl;
+            return false;
+        }
+    }
+    cout << "Palindrom" << endl;
+    return true;
+}
+/*
+int Graham() {
+    int k;
+    cout << "Input point's amount: ";
+    cin >> k;
+    int * arr = new int [int];
+
+} */
+
+int main() {
+    int N;
+    while (true) {
+        cout << "Type task's number: ";
+        cin >> N;
+        switch (N) {
+        case 1:
+            palindrom();
+            break;
+        case 2:
+            break;
+        case 3:
+            cout << "Goodbye" << endl;
+            return 0;
+        default:
+            cout << "Wrong input. Try again (1 - 3)." << endl;
+            break;
+        }
+    }
+    return 0;
+}
