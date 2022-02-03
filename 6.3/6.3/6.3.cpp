e738a555051f818b6abc857596aa73d192ef1bee
popidiot <90963991+popidiot@users.noreply.github.com>
2022-02-04 02:09:02 +0300

    Add files via upload

---
+++ 6.3/6.3/6.3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+using namespace std;
+
+unsigned calculate(int b) {
+    while (b % 5 == 0)
+        b /= 5;
+    while (b % 3 == 0)
+        b /= 3;
+    while (b % 2 == 0)
+        b /= 2;
+    return b;
+}
+
+int main() {
+    while (true) {
+        int point = 2, count = 0, num;
+        cin >> num;
+        if (num == 0) break;
+        while (count < num) {
+            if (calculate(point) == 1) {
+                cout << point << " ";
+                ++count;
+            }
+            ++point;
+        }
+        cout << endl;
+    }
+    return 0;
+}
